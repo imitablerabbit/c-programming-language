ee232b6d010d79638c0dced7be1227a5dee1a743
Mark Hillman <markhillman94@googlemail.com>
2019-03-29 18:55:48 +0000

    Change variable and function names

--- chapter1/detab.c
+++ chapter1/detab.c
@@ -3,22 +3,23 @@
 #define TAB_STOP 8
 #define MAX_LINE_LENGTH 1000
 
-int getLine(char[], int);
-void replaceTabs(char[], int, char[], int);
+int getline(char[], int);
+void replacetabs(char[], int, char[], int);
 
 int main() {
     char line[MAX_LINE_LENGTH];
     int len;
-    while ((len = getLine(line, MAX_LINE_LENGTH)) > 0) {
+    while ((len = getline(line, MAX_LINE_LENGTH)) > 0) {
         printf("Line: %s", line);
-        char expandedLine[len * TAB_STOP];
-        replaceTabs(line, len, expandedLine, TAB_STOP);
-        printf("Expanded Line: %s", expandedLine);
+        char expandedline[len * TAB_STOP];
+        replacetabs(line, len, expandedline, TAB_STOP);
+        printf("Expanded Line: %s", expandedline);
     }
     return 0;
 }
 
-int getLine(char a[], int lim) {
+// Get the next line from stdin
+int getline(char a[], int lim) {
     int c, i;
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
         a[i] = c;
@@ -33,7 +34,7 @@ int getLine(char a[], int lim) {
 
 // Replace the tabs in the string with the number of
 // spaces needed to meet the tab stop
-void replaceTabs(char from[], int size, char to[], int tabStop) {
+void replacetabs(char from[], int size, char to[], int tabstop) {
     int c, i, j;
     int spaces; // Number of spaces to replace the tab with
     int offset = 0; // The current offset from the original
@@ -43,7 +44,7 @@ void replaceTabs(char from[], int size, char to[], int tabStop) {
             to[i + offset] = c;
             continue;
         }
-        spaces = tabStop - ((i + offset) % tabStop);
+        spaces = tabstop - ((i + offset) % tabstop);
         for (j = i + offset; j < i + offset + spaces; j++)
             to[j] = ' ';
         offset += spaces - 1;

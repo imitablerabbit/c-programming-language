9c516e460d5730950ef2a34be6b03286c5a65324
Mark Hillman <markhillman94@googlemail.com>
2019-07-16 00:25:41 +0100

    Update expand.c

--- chapter3/expand.c
+++ chapter3/expand.c
@@ -1,51 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
-#define BUFFER_SIZE 1000
+/* TODO: Fix '-' characters getting dropped.
+ * TODO: Move array appending into new function.
+ * */
 
-/* TODO: Fix the duplicate characters when chaining expansions. 
- * TODO: Fix dropped alnum when they are not followed by a '-'*/
-
-int expand(char* s1, char* s2);
+int expand(char* expansion_chars, char** expanded);
 
 int main() {
-    char* s1 = "aa-z\n0-3-4\n5-7-910-\ng-n\n";
-    char s2[BUFFER_SIZE];
+    char* s = "az";
+    char* expanded;
+    expand(s, &expanded);
+    printf("%s\n", expanded);
+    free(expanded);
 
-    expand(s1, s2);
-    printf("%s\n", s2);
+    expand_string("a-z1-3-5-9g-m", 11, &expanded);
+    printf("%s\n", expanded);
+    free(expanded);
 }
 
-int expand(char* s1, char* s2) {
-    int pos; /* The current position of the new string */
-    int i; 
+/* Parse the string and expand when we need to. */
+int expand_string(char* expansion_string, int len, char** expanded) {
+    int has_expanded;
+    
+    char* buffer;
+    char* buffer_temp;
+    int buffer_size;
 
-    /* The current and ending characters of the expansion. */
-    char current, end; 
+    char prev, prev2;
+    int i;
+    int char_count;
+    char expansion_chars[1000];
+    char* expanded_temp;
+    int expansion_count;
 
-    pos = 0;
-    for (i = 0; s1[i] != '\0'; i++) {
-        if (isalnum(s1[i])) {
-            
-            /* Keep expanding until we no longer match the chars */
-            while (s1[i+1] == '-' && isalnum(s1[i+2])) {
-                current = s1[i];
-                end = s1[i+2];
+    /* Check that there are enough chars to expand. */
+    has_expanded = 0;
+    if (len < 4)
+        return has_expanded;
 
-                /* Perform the character expanding. */
-                while (current <= end) {
-                    s2[pos] = current;
-                    current++;
-                    pos++;
+    /* Initialise with a default allocation. */
+    buffer = (char*) malloc(len);
+    buffer_size = len;
+
+    /* Loop through the characters and expand them either side of
+     * the '-' chars. */
+    char_count = 0;
+    prev = *expansion_string++;
+    while (*expansion_string) {
+        if (*expansion_string == '-') {
+            /* Loop until we reach 2 characters which are not '-'. */
+            i = 0;
+            expansion_chars[i] = prev;
+            i++;
+            while (*expansion_string) {
+                if (prev != '-' && *expansion_string != '-') {
+                    break;
                 }
-                i += 2;
+                if (*expansion_string != '-') {
+                    expansion_chars[i] = *expansion_string;
+                    i++;
+                }
+                prev2 = prev;
+                prev = *expansion_string++;
             }
+            expansion_chars[i] = '\0';
+            expansion_count = expand(expansion_chars, &expanded_temp);
+            has_expanded = 1;
+
+            /* Add the expanded chars to current character list. Memory is
+             * reallocated on each expansion to encompass only the amount of
+             * characters needed. */
+            buffer_size = buffer_size + expansion_count;
+            buffer_temp = malloc(buffer_size);
+            buffer_temp = strcat(buffer_temp, buffer);
+            buffer_temp = strcat(buffer_temp, expanded_temp);
+            buffer = buffer_temp;
+            free(expanded_temp);
         } else {
-            s2[pos] = s1[i];
-            pos++;
+            *buffer = *expansion_string;
+            char_count++;
         }
+        prev = *expansion_string++;
     }
-    return 0;
+    *expanded = buffer;
+    return has_expanded;
 }
 
+/* Expand the differences between the chars.
+ *
+ * The number of characters that were expanded is returned from the function.
+ * The expansion is applied to the passing in character array using the `exp'
+ * characters. */
+int expand(char* expansion_chars, char** expanded) {
+    char* expansion_chars_start = expansion_chars;
+    size_t buffer_size;
+    char* buffer;
+    char* buffer_start;
+    char prev;
+    char c;
 
+    /* Calculate the size of the string buffer needed to expand
+     * the characters. */
+    buffer_size = 1;
+    prev = *expansion_chars++;
+    while (*expansion_chars) {
+        buffer_size += *expansion_chars - prev;
+        prev = *expansion_chars;
+        expansion_chars++; 
+    }
+    buffer_size++; /* Need space for null ending. */
+    buffer = (char*) malloc(buffer_size);
+    buffer_start = buffer;
+
+    /* Expand the characters into the buffer. We only add the end expansion
+     * character when at the very end so as to avoid duplicating some of
+     * the characters. */
+    expansion_chars = expansion_chars_start;
+    prev = *expansion_chars++;
+    while (*expansion_chars) {
+        for (c = prev; c < *expansion_chars; c++) {
+            *buffer++ = c;
+        }
+        prev = *expansion_chars;
+        expansion_chars++;
+    }
+    *buffer++ = prev;
+    *buffer = 0;
+    *expanded = buffer_start;
+    return buffer_size;
+}
